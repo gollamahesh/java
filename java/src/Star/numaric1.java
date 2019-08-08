b0dd01f15a26022262270a8af5a3f983e2c3660d
imgadmin <imgadmin@10.154.250.16>
2019-08-08 13:58:06 +0530

    changed 5 to 6

--- java/src/Star/numaric1.java
+++ java/src/Star/numaric1.java
@@ -5,7 +5,7 @@ public class numaric1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i=1;i<=5;i++)
+		for(int i=1;i<=6;i++)
 		{
 			for(int j=1;j<=i;j++)
 			{
